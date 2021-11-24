3d68f0fbbed48a37581df071159610aba7fa93a9
yaojia25 <yaojia0225@gmail.com>
2021-11-25 01:24:34 +0800

    change to bspwm,add bspwm and sxhkd config

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,17 @@
+# Wallpaper
+feh --bg-fill $HOME/Pictures/wallpaper.png &
+
+# 渲染器
+picom &
+
+# polybar
+~/.config/polybar/launch.sh &
+
+# fcitx5
+fcitx5 -d &
+
+# 网络管理GUI
+nm-applet &
+
+# 通知
+dunst &
