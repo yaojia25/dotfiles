a90c8518d996bfbdfce37d0ba1c802900307717f
Jasyo <31342892+yaojia25@users.noreply.github.com>
2021-12-06 21:28:12 +0800

    merge bspwm branch,backup kde config to kde branch

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,19 @@
+sxhkd &
+wmname LG3D
+# Wallpaper
+feh --bg-fill $HOME/Pictures/wallpaper.jpg &
+
+# 渲染器
+picom &
+
+# polybar
+~/.config/polybar/launch.sh &
+
+# fcitx5
+fcitx5 -d &
+
+# 网络管理GUI
+nm-applet &
+
+# 通知
+dunst &
